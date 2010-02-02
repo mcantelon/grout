d1cf2f081014f75a9ea32c0b51c5941f7219d49a
Mike Cantelon <mike@mike-cantelons-macbook.local>
2010-02-02 13:02:00 -0500

    Added numbers, etc., to grout pixel font.

--- src/grout_text.js
+++ src/grout_text.js
@@ -112,7 +112,7 @@ var grout_pixel_font = {
 
   's': ".CCCC \
         C.... \
-        .CC.. \
+        .CCC. \
         ....C \
         CCCC.",
 
@@ -158,6 +158,66 @@ var grout_pixel_font = {
         .C... \
         CCCCC",
 
+  '0': ".CCC. \
+        C...C \
+        C...C \
+        C...C \
+        .CCC.",
+
+  '1': ".CC.. \
+        ..C.. \
+        ..C.. \
+        ..C.. \
+        .CCC.",
+
+  '2': ".CCC. \
+        C...C \
+        ..CC. \
+        .C... \
+        CCCCC",
+
+  '3': ".CCC. \
+        C...C \
+        ..CC. \
+        C...C \
+        .CCC.",
+
+  '4': "C...C \
+        C...C \
+        CCCCC \
+        ....C \
+        ....C",
+
+  '5': "CCCCC \
+        C.... \
+        CCCC. \
+        ....C \
+        CCCC.",
+
+  '6': ".CCCC \
+        C.... \
+        CCCC. \
+        C...C \
+        .CCC.",
+
+  '7': "CCCCC \
+        ....C \
+        ...C. \
+        ..C.. \
+        .C...",
+
+  '8': ".CCC. \
+        C...C \
+        .CCC. \
+        C...C \
+        .CCC.",
+
+  '9': ".CCC. \
+        C...C \
+        .CCCC \
+        ....C \
+        CCCC.",
+
   '.': "..... \
         ..... \
         ..... \
@@ -174,13 +234,19 @@ var grout_pixel_font = {
         C...C \
         ..CC. \
         ..... \
-        ..C.."
+        ..C..",
+
+  ',': "..... \
+        ..... \
+        ..... \
+        ..C.. \
+        .C..."
 }
 
 var grout_pixeL_font_single_letter_data;
 
 // add tolower
-var grout_text_stamp_text = function(text, offset_x, offset_y, max_width, text_color) {
+var grout_text_stamp_text = function(text, offset_x, offset_y, max_width, text_color, pixels_between_letters) {
 
   var words = text.split(' ');
   var word;
@@ -188,6 +254,7 @@ var grout_text_stamp_text = function(text, offset_x, offset_y, max_width, text_c
   var y = 0;
 
   var text_color = this.merge(text_color, true);
+  var pixels_between_letters = this.merge(pixels_between_letters, 1);
 
   // parse pixel font into pixel arrays for speed
   for(var letter in grout_pixel_font) {
@@ -202,6 +269,11 @@ var grout_text_stamp_text = function(text, offset_x, offset_y, max_width, text_c
 
    word = words[i];
 
+   if (i > 0) {
+      // add space
+      x = x + 5 + pixels_between_letters;
+   }
+
    // each letter is 5 pixels wide with 1 pixel margin
     word_pixel_width = word.length * 6;
 
@@ -209,14 +281,14 @@ var grout_text_stamp_text = function(text, offset_x, offset_y, max_width, text_c
     if ((x + word_pixel_width) > max_width) {
       x = 0;
      // each letter is 5 pixels hight with 1 pixel margin
-      y = y + 6;
+      y = y + 5 + pixels_between_letters;
     }
 
     // stamp each letter of text
     for (var j = 0; j < word.length; j++) {
 
       this.stamp(grout_pixel_font_data[word[j]], offset_x + x, offset_y+ y);
-      x = x + 6;
+      x = x + 5 + pixels_between_letters;
     }
   }
 }
