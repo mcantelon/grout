69d2787f93f45df5ba5833f825f19d5fa8aa6484
Mike Cantelon <mike@mike-cantelons-macbook.local>
2010-02-20 00:48:45 -0500

    Added lower casing of text.

--- src/grout_text.js
+++ src/grout_text.js
@@ -260,7 +260,7 @@ var grout_pixeL_font_single_letter_data;
 // add tolower
 var grout_text_stamp_text = function(text, offset_x, offset_y, max_width, text_color, pixels_between_letters) {
 
-  var words = text.split(' ');
+  var words = text.toLowerCase().split(' ');
   var word;
   var x = 0;
   var y = 0;
