e1b4fd870cbc30d42c43e1c6c4d658b47673a62f
Mike Cantelon <mike@mike-cantelons-macbook.local>
2010-03-27 00:51:05 -0400

    Code cleanup of Blood Funnel help and intro screen logic.

--- demo/blood_funnel_help_and_intro.js
+++ demo/blood_funnel_help_and_intro.js
@@ -12,12 +12,11 @@ function start_screen(grout) {
 	grout.play_sound('intro_soundtrack')
 	grout.stop_sound('game_soundtrack')
 
-	var background_pattern = help_screen_background_pattern(grout, 'start')
+	help_screen_background_pattern(grout, 'start')
+		.stamp_text('blood funnel', 5, 5, 50, '#444444')
+    	.stamp_text('blood funnel', 4, 4, 50, '#ff0000')
 
-    background_pattern.stamp_text('blood funnel', 5, 5, 50, '#444444')
-    background_pattern.stamp_text('blood funnel', 4, 4, 50, '#ff0000')
-
-	var author_credit = grout.sprite(
+	grout.sprite(
 	    'author_credit', {
 	        'group': 'start',
 	        'width': 120,
@@ -31,7 +30,7 @@ function start_screen(grout) {
 	.stamp_text('(c)2010 by mike cantelon', 1, 1, 80, '#555555')
 	.stamp_text('(c)2010 by mike cantelon', 0, 0, 80, '#888888')
 
-    var banker = grout.sprite(
+    grout.sprite(
         'banker', {
         	'group': 'start',
         	'tile_width': TILE_WIDTH * 4,
@@ -40,8 +39,7 @@ function start_screen(grout) {
             'offset_y': 7
         }
     )
-
-	banker.make_sprite(" \
+    .make_sprite(" \
 		...HHH.... \
 		...FFF.... \
 		...FFF.... \
@@ -53,14 +51,18 @@ function start_screen(grout) {
 		..N...N.BB \
 		..N...N.BB \
 		..K...K... \
-	", banker_color_map(banker_random_skin_color(), banker_random_hair_color()))
+	", banker_color_map(
+			banker_random_skin_color(),
+			banker_random_hair_color()
+		)
+	)
 
 	create_start_screen_start_button(grout)
 	create_start_screen_help_button(grout)
 
 	grout
-	  .reset_input_handling()
-	  .draw_all('start')
+	.reset_input_handling()
+	.draw_all('start')
 }
 
 function create_start_screen_start_button(grout) {
@@ -72,8 +74,7 @@ function create_start_screen_start_button(grout) {
 	        'tile_height': TILE_HEIGHT
 	    }
 	)
-
-	start_button.make_sprite(" \
+	.make_sprite(" \
 		*********************** \
 		*.....................* \
 		*..**.***..*..**..***.* \
@@ -84,17 +85,12 @@ function create_start_screen_start_button(grout) {
 		*.....................* \
 		*********************** \
 	")
+	.click_inside(function(grout) {
+		main_screen(grout)
+	})
 
 	start_button.offset_x = 7
 	start_button.offset_y = 60
-
-	// sprite click logic receives x, y in tiles
-	start_button.click(function(x, y) {
-
-		if (this.inside_margins(x, y)) {
-			main_screen(this.parent)
-		}
-	})
 }
 
 function create_start_screen_help_button(grout) {
@@ -106,8 +102,7 @@ function create_start_screen_help_button(grout) {
 	        'tile_height': TILE_HEIGHT   
 	    }
 	)
-
-	help_button.make_sprite(" \
+	.make_sprite(" \
 		******************* \
 		*.................* \
 		*.*.*.***.*...**..* \
@@ -118,17 +113,12 @@ function create_start_screen_help_button(grout) {
 		*.................* \
 		******************* \
 	")
+	.click_inside(function(grout) {
+		help_screen(grout)
+	})
 
 	help_button.offset_x = 34
 	help_button.offset_y = 60
-
-	// sprite click logic receives x, y in tiles
-	help_button.click(function(x, y) {
-
-		if (this.inside_margins(x, y)) {
-			help_screen(this.parent)
-		}
-	})
 }
 
 function create_help_text_map(grout, group) {
@@ -153,8 +143,7 @@ function create_help_back_button(grout, group, click_function) {
 	        'tile_height': TILE_HEIGHT
 	    }
 	)
-
-	help_button.make_sprite(" \
+	.make_sprite(" \
 		******************* \
 		*.................* \
 		*.**...*...**.*.*.* \
@@ -165,19 +154,14 @@ function create_help_back_button(grout, group, click_function) {
 		*.................* \
 		******************* \
 	")
+	.click_inside(function(grout) {
+		click_function(grout)
+	})
 
 	help_button.offset_x = 7
 	help_button.offset_y = 60
 
-	// sprite click logic receives x, y in tiles
-	help_button.click(function(x, y) {
-
-		if (this.inside_margins(x, y)) {
-			click_function(this.parent)
-		}
-	})
-
-	return help_button
+	//return help_button
 }
 
 function create_help_next_button(grout, group, click_function) {
@@ -189,8 +173,7 @@ function create_help_next_button(grout, group, click_function) {
 	        'tile_height': TILE_HEIGHT
 	    }
 	)
-
-	next_button.make_sprite(" \
+	.make_sprite(" \
 		******************** \
 		*..................* \
 		*.*..*.***.*.*.***.* \
@@ -201,19 +184,14 @@ function create_help_next_button(grout, group, click_function) {
 		*..................* \
 		******************** \
 	")
+	.click_inside(function(grout) {
+		click_function(grout)
+	})
 
 	next_button.offset_x = 30
 	next_button.offset_y = 60
 
-	// sprite click logic receives x, y in tiles
-	next_button.click(function(x, y) {
-
-		if (this.inside_margins(x, y)) {
-			click_function(this.parent)
-		}
-	})
-
-	return next_button
+	//return next_button
 }
 
 function help_screen(grout) {
@@ -224,16 +202,16 @@ function help_screen(grout) {
       + "vampire squid wrapped around the face of humanity, relentlessly "
       + "jamming its blood funnel into anything that smells like money.\""
 
-	var help_text_map = create_help_text_map(grout, 'help')
+	create_help_text_map(grout, 'help')
       .stamp_text(help_text, 5, 3, 140, 'black')
       .stamp_text('-Matt Taibbi', 108, 55, 70, 'black')
 
-	var help_button = create_help_back_button(grout, 'help', start_screen)
-	var next_button = create_help_next_button(grout, 'help', help_screen_2)
+	create_help_back_button(grout, 'help', start_screen)
+	create_help_next_button(grout, 'help', help_screen_2)
 
 	grout
-	  .reset_input_handling()
-	  .draw_all('help')
+	.reset_input_handling()
+	.draw_all('help')
 }
 
 function help_screen_2(grout) {
@@ -245,14 +223,14 @@ function help_screen_2(grout) {
       + "that enable them to conduct massive swindles and loot the public "
       + "coffers."
 
-	var help_text_map = create_help_text_map(grout, 'help_2').stamp_text(help_text, 5, 3, 140, 'black')
+	create_help_text_map(grout, 'help_2').stamp_text(help_text, 5, 3, 140, 'black')
 
-	var help_button = create_help_back_button(grout, 'help_2', help_screen)
-	var next_button = create_help_next_button(grout, 'help_2', help_screen_3)
+	create_help_back_button(grout, 'help_2', help_screen)
+	create_help_next_button(grout, 'help_2', help_screen_3)
 
 	grout
-	  .reset_input_handling()
-	  .draw_all('help_2')
+	.reset_input_handling()
+	.draw_all('help_2')
 }
 
 function help_screen_3(grout) {
@@ -263,14 +241,14 @@ function help_screen_3(grout) {
       + "attempt to compromise public and private institutions, you must "
       + "fight them using any means necessary."
 
-	var help_text_map = create_help_text_map(grout, 'help_3').stamp_text(help_text, 5, 3, 140, 'black')
+	create_help_text_map(grout, 'help_3').stamp_text(help_text, 5, 3, 140, 'black')
 
-	var help_button = create_help_back_button(grout, 'help_3', help_screen_2)
-	var next_button = create_help_next_button(grout, 'help_3', help_screen_4)
+	create_help_back_button(grout, 'help_3', help_screen_2)
+	create_help_next_button(grout, 'help_3', help_screen_4)
 
 	grout
-	  .reset_input_handling()
-	  .draw_all('help_3')
+	.reset_input_handling()
+	.draw_all('help_3')
 }
 
 function help_screen_4(grout) {
@@ -281,12 +259,12 @@ function help_screen_4(grout) {
       + "won't kill you if you touch them so feel free to get close to "
       + "them and shoot if they are attempting to infiltrate."
 
-	var help_text_map = create_help_text_map(grout, 'help_4').stamp_text(help_text, 5, 3, 140, 'black')
+	create_help_text_map(grout, 'help_4').stamp_text(help_text, 5, 3, 140, 'black')
 
-	var help_button = create_help_back_button(grout, 'help_4', help_screen_3)
-	var next_button = create_help_next_button(grout, 'help_4', start_screen)
+	create_help_back_button(grout, 'help_4', help_screen_3)
+	create_help_next_button(grout, 'help_4', start_screen)
 
 	grout
-	  .reset_input_handling()
-	  .draw_all('help_4')
+	.reset_input_handling()
+	.draw_all('help_4')
 }
